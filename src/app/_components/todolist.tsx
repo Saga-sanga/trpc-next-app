4ad003a4ee003a25cc9c65f69cd4fcdebabdab34
Saga-sanga <vulcan248@gmail.com>
2023-08-23 23:34:58 +0530

    Add interaction

--- src/app/_components/todolist.tsx
+++ src/app/_components/todolist.tsx
@@ -1,10 +1,41 @@
-"use client"
-import { trpc } from "../_trpc/client"
+"use client";
+import { trpc } from "../_trpc/client";
+import { useState } from "react";
 
 export default function TodoList() {
+  const utils = trpc.useContext();
   const getTodos = trpc.getTodos.useQuery();
+  const addTodo = trpc.addTodo.useMutation({
+    onSettled: () => utils.getTodos.invalidate(),
+  });
+  const [content, setContent] = useState("");
+
+  const handleSubmit = async () => {
+    if (content.length) {
+      addTodo.mutate(content);
+      setContent("");
+    }
+  }
 
   return (
-    <div>{JSON.stringify(getTodos.data)}</div>
-  )
+    <div>
+      <div>{JSON.stringify(getTodos.data)}</div>
+      <div>
+        <label htmlFor="content">Content</label>
+        <input
+          id="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" ? handleSubmit() : null}
+          className="text-black"
+          type="text"
+        />
+        <button
+          onClick={handleSubmit}
+        >
+          Add Todo
+        </button>
+      </div>
+    </div>
+  );
 }
