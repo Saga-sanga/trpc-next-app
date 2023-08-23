bef634a88935fa8e18f9b12df86fa6c53506e1b4
Saga-sanga <vulcan248@gmail.com>
2023-08-23 22:37:26 +0530

    First commit

---
+++ src/app/_components/todolist.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { trpc } from "../_trpc/client"
+
+export default function TodoList() {
+  const getTodos = trpc.getTodos.useQuery();
+
+  return (
+    <div>{JSON.stringify(getTodos.data)}</div>
+  )
+}
